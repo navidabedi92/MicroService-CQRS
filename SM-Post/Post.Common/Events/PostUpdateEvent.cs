ccb58d3665612c2a34c3fe01c92fe6693244727a
intelejentboy <32359711+intelejentboy@users.noreply.github.com>
2025-05-07 19:54:39 +0330

    add events

---
+++ SM-Post/Post.Common/Events/PostUpdateEvent.cs
@@ -0,0 +1,14 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class PostUpdateEvent : BaseEvent
+    {
+        public PostUpdateEvent() : base(nameof(PostUpdateEvent))
+        {
+
+        }
+
+        public string Message { get; set; }
+    }
+}
